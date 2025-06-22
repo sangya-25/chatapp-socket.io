e8e385e28435525fc8a9491ca493a92761cbb33c
sangya-25 <ojha.sangya25@gmail.com>
2025-06-22 23:05:43 +0530

    few updates

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const {authUser, loading}=useContext(AuthContext)
   if (loading) return null; // Prevents double loading indicator
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain ">
+    <div className="bg-[url('/bgImage.svg')] bg-contain ">
       <Toaster/>
       <Routes>
         <Route path='/' element={authUser ?<HomePage/>:<Navigate to='/login'/>}/>
--- server/server.js
+++ server/server.js
@@ -47,5 +47,8 @@ app.use('/api/messages',messageRouter);
 
 //Connect to Mongodb
 await connectDB();
-const PORT = process.env.PORT||5000;
-server.listen(PORT, ()=> console.log('Server is running on port:'+PORT));
+if(process.env.NODE_ENV !=="production"){
+    const PORT = process.env.PORT||5000;
+    server.listen(PORT, ()=> console.log('Server is running on port:'+PORT));
+}
+export default server;
